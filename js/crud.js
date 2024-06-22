22f9d68b238352ba22b7975dacf5bcbbc539d97f
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-06-22 18:14:14 -0500

    feat: :construction: adventages in fuction js

--- js/crud.js
+++ js/crud.js
@@ -1,3 +1,5 @@
+// Add
+
 export const AddToDo = async (arg) => {
     let val = await validarAgregarTarea(arg);
     if (val) return val;
@@ -16,6 +18,40 @@ export const AddToDo = async (arg) => {
 
 const validarAgregarTarea = async (act) => {
     if (typeof act.task !== "string" || act.task === undefined) {
-        return { status: 406, message: "Los datos de la tarea no estÃ¡n llegando correctamente" };
+        return { status: 406, message: "Los datos de la tarea no estan llegando correctamente" };
     }
 };
+
+// Get
+
+export const getAllPendingTasks = async()=>{
+    console.log("Esperando .......");
+    const url = `https://66760dffa8d2b4d072f2497f.mockapi.io/toDoList`;
+    const options = {
+        method: 'GET',
+        headers: { "Content-type": "application/json" }
+    };
+    let res = await fetch(url, options);
+    let data = await res.json();
+
+
+    let filteredData = data.filter(item => item.status === "On hold");
+
+    return filteredData;
+}
+
+export const getAllCompleteTasks = async()=>{
+    console.log("Esperando .......");
+    const url = `https://66760dffa8d2b4d072f2497f.mockapi.io/toDoList`;
+    const options = {
+        method: 'GET',
+        headers: { "Content-type": "application/json" }
+    };
+    let res = await fetch(url, options);
+    let data = await res.json();
+
+
+    let filteredData = data.filter(item => item.status === "ready");
+
+    return filteredData;
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-import { AddToDo } from "./crud.js";
+import { AddToDo, getAllCompleteTasks, getAllPendingTasks } from "./crud.js";
+import { taskPending, taskComplete} from "./tasks.js";
 
-const agregarTarea = async () => {
-    let act = {};
-    act.task = prompt("Ingrese la tarea:");
-    act.status = "On Hold";
-    let resultado = await AddToDo(act);
-    console.log(resultado);
-};
+// const agregarTarea = async () => {
+//     let act = {};
+//     act.task = prompt("Ingrese la tarea:");
+//     act.status = "On Hold";
+//     let resultado = await AddToDo(act);
+//     console.log(resultado);
+// };
 
-agregarTarea();
+// agregarTarea();
+
+let onHold = document.querySelector(".onHold")
+let complete = document.querySelector(".complete")
+
+addEventListener("DOMContentLoaded", async()=>{
+    let infoPend = await getAllPendingTasks()
+    console.log(infoPend);
+
+    onHold.innerHTML = await taskPending(infoPend)
+
+    let infoComp = await getAllCompleteTasks()
+    console.log(infoComp);
+
+    complete.innerHTML = await taskComplete(infoComp)
+
+})
